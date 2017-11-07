14bfb9d7637ac87d402ef6efd673d04dea33afad
antonius177 <31312836+antonius177@users.noreply.github.com>
2017-11-07 16:30:11 +0700

    level 13.1

--- app/User.php
+++ app/User.php
@@ -23,4 +23,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+	public function roles(){
+		return $this->belongsToMany('App\Role');
+	}
 }
